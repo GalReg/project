64ec935098ac6c8beb3d7f36e7ddeea09656884e
GalReg <77444498+GalReg@users.noreply.github.com>
2022-02-15 19:52:56 +0300

    Add files via upload

---
+++ project_general/front/node/front-development/styleguide.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Личный кабинет. Документация к клиентской части',
+  ignore: [
+    '**/components/ProjectPage/StatisticsCard/StatisticsCard.vue',
+    '**/components/ProjectPage/NewsArticles/NewsArticles.vue'
+  ],
+  exampleMode: 'expand',
+  tocMode: 'expand',
+  usageMode: 'expand',
+  showSidebar: false
+}
